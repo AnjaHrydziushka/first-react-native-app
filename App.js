a65b95431c07eb91ce5162670ea5ab959d743d41
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-26 14:12:01 +0200

    Made an awesome color change with a motion senser

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from "react";
-import { View } from "react-native";
+import GameScreen from "./src/screens/GameScreen";
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-    }}>
-      <View
-        style={{ width: 50, height: 70, backgroundColor: "rebeccapurple" }}
-      />
-      <View style={{ width: 70, height: 70, backgroundColor: "crimson" }} />
-      <View style={{ width: 90, height: 70, backgroundColor: "gold" }} />
-    </View>
-  );
+  return <GameScreen />;
 }
