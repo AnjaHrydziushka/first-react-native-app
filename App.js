151f39366d7333239e7e25fcafdef4d628f860d4
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-26 15:55:56 +0200

    Created simple navigation

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import GameScreen from "./src/screens/GameScreen";
+import HomeScreen from "./src/components/HomeScreen";
 
-export default function App() {
-  return <GameScreen />;
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
