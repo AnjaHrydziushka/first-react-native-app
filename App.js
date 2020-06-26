dca0da3d59da7e05554309936c71d4e195b8a8b3
AnjaHrydziushka <anja.hryd@gmail.com>
2020-06-26 12:19:47 +0200

    Used TouchableOpacity and FlatList

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useRef } from "react";
-import { Text, View, ActivityIndicator, Image, Button, Alert, ScrollView } from "react-native";
+import React, { useRef, useState } from "react";
+import { Text, View, ActivityIndicator,
+    Image, Button, Alert,
+    TouchableOpacity, TextInput, 
+    FlatList } from "react-native";
 
-export default function App() {
-  const randomNum = useRef(Math.random()).current;
-
+function MyHeader() {
   return (
     <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-      <ScrollView>
       <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
         Hello React Native
       </Text>
@@ -15,17 +15,14 @@ export default function App() {
         color="#c1262c"
         style={{ marginBottom: 30 }}
       />
-          {[0, 1, 2, 3, 4].map(i => {
-      return (
-        <Image
-          key={i}
-          source={{
-            uri: `https://picsum.photos/500/300?random=${randomNum + i}`
-          }}
-          style={{ width: "100%", height: 160, marginBottom: 30 }}
-        />
-      );
-    })}
+    </View>
+  )
+}
+    
+function MyFooter() {
+  const [smth, setSmth] = useState("")
+  return (
+    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
       <View
         style={{
           borderWidth: 2,
@@ -35,13 +32,80 @@ export default function App() {
         }}
       >
         <Text>Hello again!</Text>
-        <Button
+        
+      </View>
+      <Button
           onPress={() => Alert.alert("Learning RN is so easy")}
           title="Learn More"
           color="#c1262c"
 />
-      </View>
-      </ScrollView>
+        <TextInput
+        value={smth}
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(text) => (setSmth(text))}
+        />
+    
     </View>
+  )
+}
+
+export default function App() {
+  const randomNum = useRef(Math.random()).current;
+
+  // const [smth, setSmth] = useState("")
+
+  return (
+    // <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+    //   <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+    //     Hello React Native
+    //   </Text>
+    //   <ActivityIndicator
+    //     size="large"
+    //     color="#c1262c"
+    //     style={{ marginBottom: 30 }}
+    //   />
+      <FlatList
+        ListHeaderComponent={MyHeader}
+        data={[0, 1, 2, 3, 4]}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+            >
+              <Image
+                source={{
+                  uri: `https://picsum.photos/500/300?random=${randomNum +
+                    item}`
+                }}
+                style={{ width: "100%", height: 160, marginBottom: 30 }}
+              />
+            </TouchableOpacity>
+          );
+        }}
+        keyExtractor={item => String(item)}
+        ListFooterComponent={MyFooter}
+      />
+//       <View
+//         style={{
+//           borderWidth: 2,
+//           borderColor: "black",
+//           padding: 20,
+//           marginBottom: 30
+//         }}
+//       >
+//         <Text>Hello again!</Text>
+        
+//       </View>
+//       <Button
+//           onPress={() => Alert.alert("Learning RN is so easy")}
+//           title="Learn More"
+//           color="#c1262c"
+// />
+//         <TextInput
+//         value={smth}
+//         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+//         onChangeText={(text) => (setSmth(text))}
+//         />
+//     </View>
   );
 }
